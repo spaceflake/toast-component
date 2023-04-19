c6cf95a40a1c8bc255e99d3d75ba2b3d87ffb7cc
spaceflake <vindicated@hotmail.com>
2023-04-19 18:10:41 +0200

    Update ToastProvider.js

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,20 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === 'Escape') {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   const handleToastClose = (id) => {
     const nextToasts = toasts.filter((toast) => toast.id !== id);
     setToasts(nextToasts);
